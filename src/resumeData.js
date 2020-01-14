daa136b414e8dc48f520020e7d99396d2055c470
Davis Hochstatter <davishochs@gmail.com>
2020-01-14 00:07:03 -0600

    portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://imgur.com/a/HzcVzSp/",
+    "name": "Davis Hochstatter",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"davishochs [at] gmail.com",
+    "skypeid": "",
+    "roleDescription": "Building full stack web applications, no matter the challenge",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/davis-hochstatter",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/DavisHochs",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/davishochs",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a Operations Manager for Techson IP and recent graduate of the UT Austin Full Stack Web Development course.",
+    "address":"",
+    "website":"https://github.com/DavisHochs",
+    "education":[
+      {
+        "UniversityName":"University of Texas Full Stack Web Developement Bootcamp",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Techson IP",
+        "specialization":"Operations Manager",
+        "MonthOfLeaving":"November",
+        "YearOfLeaving":"2019 - Present",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Skills",
+    "skills":[
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"React"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"LinkUP",
+        "description":"Social Media Party Up App",
+        "imgurl":"images/linkup.png",
+        "imglink":"https://stormy-sands-04554.herokuapp.com/"
+      },
+      {
+        "name":"React Clicking Game",
+        "description":"Reactjs Simple Memory Game",
+        "imgurl":"images/clicking.png",
+        "imglink":"https://davishochs.github.io/ClickingGame/"
+      },
+      {
+        "name":"Amazon Type CLI Application",
+        "description":"CLI Application",  
+        "imgurl":"images/bamazon.png",
+        "imglink":"https://github.com/DavisHochs/Bamazon"
+      },
+      {
+        "name":"Multiplayer Rock Paper Scissors Game",
+        "description":"RPS With a Friend",
+        "imgurl":"images/rps.png",
+        "imglink":"https://davishochs.github.io/MP-RPS/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
